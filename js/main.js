9aaa195e77f34eee0625ca808752aa37ebfc3c67
AlbertoMessineo <albevorox@gmail.com>
2023-11-15 17:23:21 +0100

    feat: js function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// FUNCTIONS:
+// board generator:
+
+function boardGen(){
+const elementBoard = document.querySelector(div.board);
+elementBoard.classList.add('board');
+
+    for( let i= 1; i <100; i++){
+
+        const elementCell = document.createElement('div');    
+        elementBoard.append(elementCell);
+        elementBox.classList.add("cell");
+    }
+}
+boardGen();
